36db6311e9d692cfac0a37d64ed4bfe5b5406404
Danylo Denysov <danyldezwaan@hotmail.com>
2019-02-20 12:34:46 +1000

    changed the react logo with dingo logo

--- dingo-client/src/App.js
+++ dingo-client/src/App.js
@@ -7,18 +7,21 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src='http://www.dingo.com/Dingo/media/img/dingo-logo.png' className="App-logo" alt="logo" />
+
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {/* Edit <code>src/App.js</code> and save to reload. */}
           </p>
-          <a
+          <p>Hey Colin we have been expecting you ;)</p>
+
+          {/* <a
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
           >
             Learn React
-          </a>
+          </a> */}
         </header>
       </div>
     );
