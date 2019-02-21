e658094f755e4bd9c6d43c0a750548b19a20e1f6
Danylo Denysov <danyldezwaan@hotmail.com>
2019-02-21 16:52:27 +1000

    added styling for the landing page

---
+++ dingo-client/src/components/BarChart.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+class BarChart extends React.Component {
+
+
+    render() {
+        return (
+            <canvas id="myCanvas" width="400" height="200">
+
+            </canvas>
+
+        )
+    }
+}
+
+export default BarChart
