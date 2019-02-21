875cb572688a8244a310d6731ae468103dccdb42
Danylo Denysov <danyldezwaan@hotmail.com>
2019-02-22 09:18:01 +1000

    Added chart component with dummy data

--- dingo-client/src/components/BarChart.js
+++ dingo-client/src/components/BarChart.js
@@ -1,15 +1,31 @@
 import React from 'react'
-
+import { Bar } from 'react-chartjs-2'
 
 class BarChart extends React.Component {
+    constructor(props) {
+        super(props)
 
+        this.state = {
+            chartData: {
+                labels: [
+                    '0 - 999',
+                    '1000 - 1999',
+                    '2000 - 2999',
+                    '3000 - 3999'
+                ],
+                datasets: [{
+                    label: 'Component life span',
+                    data: [3, 4, 2, 5],
+                    backgroundColor: 'red'
+                }]
+
+            }
+        }
+    }
 
     render() {
         return (
-            <canvas id="myCanvas" width="400" height="200">
-
-            </canvas>
-
+            <Bar data={this.state.chartData} />
         )
     }
 }
