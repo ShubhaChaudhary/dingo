4f17ca56abf300bcebecd4bc597c982e9c7caf6c
Shubha Chaudhary <shubha.learning@gmail.com>
2019-02-24 00:15:33 +1000

    added port

--- dingo-server/server.js
+++ dingo-server/server.js
@@ -20,6 +20,7 @@ app.get('/', (req, res) => {
     res.status(200).send("Dingo Dashboard")
 })
 
+const PORT = process.env.PORT || 3001
 
 
 app.listen(PORT, () => console.log('App listening on port 3001!'))
