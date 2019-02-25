85128cd2b83c3839db07cb883c8038f43768889a
Shubha Chaudhary <shubha.learning@gmail.com>
2019-02-25 13:50:56 +1000

    modified route for landing page

--- dingo-server/routes/data.js
+++ dingo-server/routes/data.js
@@ -2,25 +2,14 @@ const express = require('express')
 const router = express.Router();
 const Data = require('../model/DataModel')
 
-
-// router.get('/', (req, res) => {
-//   Data.find().then((result)=>{
-//      console.log(res.send({result}))
-//   }).catch(
-//      error => res.status(500).json({
-//          error: error.message
-//      })
-//  )
-// })
-
 router.get('/',(req,res)=>{
-    Data.aggregate( [ 
-        { $match : { Site : "Newmont Nevada" } },
+ Data.aggregate( [ 
+        { $match : { Site :req.body.site } },
       {
         $bucket: {
           groupBy: "$Component Age",
           boundaries: [ 0, 5000, 10000, 15000, 20000, 25000, 30000 ],
-            default:"Others",
+            default:"> 30000",
           output: {
             "count": { $sum: 1 },
             "age" : { $push: "$Component Age" }
