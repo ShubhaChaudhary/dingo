8fa5517cdfacb720069f41e7dab6d83799f7532b
Danylo Denysov <danyldezwaan@hotmail.com>
2019-02-26 15:02:34 +1000

    NVD3 bar chart

--- dingo-client/src/components/BarChart.js
+++ dingo-client/src/components/BarChart.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Bar } from 'react-chartjs-2'
-
+import NVD3Chart from 'react-nvd3'
 class BarChart extends React.Component {
     constructor(props) {
         super(props)
@@ -23,19 +23,54 @@ class BarChart extends React.Component {
         }
     }
 
-    render() {
-        return (
-            <Bar data={this.state.chartData} options={{
 
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
+    datum = [
+        {
+            key: "Cumulative Return",
+            values: [
+                {
+                    "label": "A",
+                    "value": -29.765957771107
+                },
+                {
+                    "label": "B",
+                    "value": 0
+                },
+                {
+                    "label": "C",
+                    "value": 32.807804682612
+                },
+                {
+                    "label": "D",
+                    "value": 196.45946739256
+                },
+                {
+                    "label": "E",
+                    "value": 0.19434030906893
+                },
+                {
+                    "label": "F",
+                    "value": -98.079782601442
+                },
+                {
+                    "label": "G",
+                    "value": -13.925743130903
+                },
+                {
+                    "label": "H",
+                    "value": -5.1387322875705
                 }
+            ]
+        }
+    ]
 
-            }} />
+
+
+    render() {
+
+
+        return (
+            <NVD3Chart id="barChart" type="discreteBarChart" datum={this.datum} x="label" y="value" />
         )
     }
 }
