15c3d65d6a03741d84efcbf1060afaacb7e526e1
Shubha Chaudhary <shubha.learning@gmail.com>
2019-02-26 11:28:05 +1000

    Merging

--- dingo-server/routes/data.js
+++ dingo-server/routes/data.js
@@ -1,15 +1,43 @@
 const express = require('express')
 const router = express.Router();
+
+
 const Data = require('../model/DataModel')
 
-router.get('/', (req, res) => {
-     Data.find().then((site)=>{
-        console.log(res.send({site}))
-     }).catch(
-        error => res.status(500).json({
-            error: error.message
+router.get('/performance', (req, res) => {
+    Data.aggregate(
+        [
+        { "$match" : {
+            "Location" : "Off Contract Trucks", 
+            "Global Asset Make" : "Caterpillar", 
+            "Global Component Model" : "793C", 
+            "Global Asset Type" : "Off-Highway Truck", 
+            "Global Asset Model" : "793C", 
+            "Global Component Type" : "Transmission"
+        }
+        }, 
+        { 
+            "$group" : {
+                "_id" : {
+                    "Component Profile" : "$Component Profile", 
+                    "RemoveDate" : "$RemoveDate"
+                }, 
+                "AVG(Component Age)" : {
+                    "$avg" : "$Component Age"
+                }
+            }
+        }, 
+      
+        { 
+            "$sort" : {
+                "RemoveDate" : 1
+            }
+        }
+    ]).then((result)=>{
+            res.send(result)
+        }).catch((e)=>{
+            res.send(e)
         })
-    )
 })
 // router.post('/',async (req,res)=>{
 //     const body=await (req.body)
