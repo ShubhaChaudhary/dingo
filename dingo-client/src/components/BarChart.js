4212327899708c97b9b554c0659a8650612c100d
Danylo Denysov <danyldezwaan@hotmail.com>
2019-03-04 14:09:32 +1000

    The data is being fetched aynchronously from the server

--- dingo-client/src/components/BarChart.js
+++ dingo-client/src/components/BarChart.js
@@ -1,75 +1,33 @@
 import React from 'react'
-// import { Bar } from 'react-chartjs-2'
 import NVD3Chart from 'react-nvd3'
+import axios from 'axios'
+import store from '../redux/store'
+import { setUserBenchmarkData } from '../redux/action'
+
+
 class BarChart extends React.Component {
-    // constructor(props) {
-    //     super(props)
-
-    //     this.state = {
-    //         chartData: {
-    //             labels: [
-    //                 '0 - 999',
-    //                 '1000 - 1999',
-    //                 '2000 - 2999',
-    //                 '3000 - 3999',
-    //             ],
-    //             datasets: [{
-    //                 label: 'Component life span',
-    //                 data: [3, 4, 14, 5],
-    //                 backgroundColor: '#313de8'
-    //             }]
-
-    //         }
-    //     }
-    // }
-
-
-    datum = [
-        {
-            key: "Cumulative Return",
-            values: [
-                {
-                    "label": "A",
-                    "value": -29.765957771107
-                },
-                {
-                    "label": "B",
-                    "value": 0
-                },
-                {
-                    "label": "C",
-                    "value": 32.807804682612
-                },
-                {
-                    "label": "D",
-                    "value": 196.45946739256
-                },
-                {
-                    "label": "E",
-                    "value": 0.19434030906893
-                },
-                {
-                    "label": "F",
-                    "value": -98.079782601442
-                },
-                {
-                    "label": "G",
-                    "value": -13.925743130903
-                },
-                {
-                    "label": "H",
-                    "value": -5.1387322875705
-                }
-            ]
+    constructor(props) {
+        super(props)
+        this.state = {
+            datum: []
         }
-    ]
 
+    }
 
+    async fetchData() {
+        const res = await axios.get('http://localhost:3001/data/dashboard')
+        const { data } = await res
+        this.setState({ datum: [{ key: "Component Health", values: data }] })
+    }
+
+    componentDidMount() {
+        this.fetchData()
+    }
 
     render() {
 
         return (
-            <NVD3Chart id="barChart" type="discreteBarChart" datum={this.datum} x="label" y="value" />
+            <NVD3Chart id="barChart" type="discreteBarChart" datum={this.state.datum} x="_id" y="count" />
         )
     }
 }
--- dingo-client/src/components/LineChart.js
+++ dingo-client/src/components/LineChart.js
@@ -1,60 +1,59 @@
 import React from 'react'
 // import { Line } from 'react-chartjs-2'
 import NVD3Chart from 'react-nvd3'
-
+import axios from 'axios'
 class LineChart extends React.Component {
-  // constructor(props) {
-  //     super(props)
+  constructor(props) {
+    super(props)
 
-  //     this.state = {
-  //         chartData: {
-  //             labels: [
-  //                 '0 - 999',
-  //                 '1000 - 1999',
-  //                 '2000 - 2999',
-  //                 '3000 - 3999',
-  //             ],
-  //             datasets: [{
-  //                 label: 'Component life span',
-  //                 data: [3, 4, 14, 5],
-  //                 backgroundColor: '#313de8'
-  //             }]
-
-  //         }
-  //     }
-  // }
-
-
-  datum = function () {
-    var sin = [],
-      cos = [];
-
-    for (var i = 0; i < 100; i++) {
-      sin.push({ x: i, y: Math.sin(i / 10) });
-      cos.push({ x: i, y: .5 * Math.cos(i / 10) });
+    this.state = {
+      datum: []
     }
-
-    return [
-      {
-        values: sin,
-        key: 'Sine Wave',
-        color: '#ff7f0e'
-      },
-      {
-        values: cos,
-        key: 'Cosine Wave',
-        color: '#2ca02c'
-      }
-    ];
   }
 
+  // datum = function () {
+  //   var sin = [],
+  //     cos = [];
 
+  //   for (var i = 0; i < 100; i++) {
+  //     sin.push({ x: i, y: Math.sin(i / 10) });
+  //     cos.push({ x: i, y: .5 * Math.cos(i / 10) });
+  //   }
+
+  //   return [
+  //     {
+  //       values: sin,
+  //       key: 'Sine Wave',
+  //       color: '#ff7f0e'
+  //     },
+  //     {
+  //       values: cos,
+  //       key: 'Cosine Wave',
+  //       color: '#2ca02c'
+  //     }
+  //   ];
+  // }
+
+  // This async function fetches and pupulates the chart on the later date.
+  // It fixed the bug where you had to press twice on the tab to get chart pupulated.
+  async fetchData() {
+    // fetch data from the server
+    const res = await axios.get('http://localhost:3001/data/dashboard')
+    // extract the payload
+    const { data } = await res
+    // insert the payload ninto the state.
+    this.setState({ datum: [{ key: "Component Health", values: data }] })
+  }
+
+  componentDidMount() {
+    this.fetchData()
+  }
 
   render() {
 
 
     return (
-      <NVD3Chart id="lineChart" type="lineChart" datum={this.datum} x="label" y="value" />
+      <NVD3Chart id="lineChart" type="lineChart" datum={this.state.datum} x="_id" y="count" />
     )
   }
 }
