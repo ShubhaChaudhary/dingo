3dd53ae77e0bded67024fe0689a1d2350a0ea754
Shubha Chaudhary <shubha.learning@gmail.com>
2019-03-06 14:09:20 +1000

    dashboard data with modification(year)

--- dingo-server/controller/dataController.js
+++ dingo-server/controller/dataController.js
@@ -1,24 +1,62 @@
 module.exports = (Data) => {
     //  Retrieves the Component Age and total count from mongodb Atlas
-    const dashboard = (req, res) => {
-        Data.aggregate([
-            { $match: { $or: [{ Site: 'Newmont Nevada' }, { Site: 'BMA Saraji' }] } },
-            {
-                $bucket: {
-                    groupBy: "$Component Age",
-                    boundaries: [0, 5000, 10000, 15000, 20000, 25000, 30000],
-                    default: "30000 <",
-                    output: {
-                        "count": { $sum: 1 }
-                    }
-                }
+    const dashboard =  (req, res) => {
+       Data.aggregate([
+          { 
+                    "$match" : {
+                        
+                                "Site" : "Newmont Nevada",
+                       "$or":[{ "RemoveDate" : 2019} ,{"RemoveDate" : 2018},{ "RemoveDate" : 2017}]
+                            
+                    }
+                },
+          { "$group": {
+            "_id": {
+              "RemoveDate": "$RemoveDate",
+              "Component Age" :
+                {
+                  $switch:
+                    {
+                      branches: [
+                        
+                        { case: { "$lt": [ "$Component Age", 5000 ] }, "then": 5000 },
+                        { "case": { "$lt": [ "$Component Age", 10000 ] }, "then": 10000 },
+                        { "case": { "$lt": [ "$Component Age", 15000 ] }, "then": 15000 },
+                        { "case": { "$lt": [ "$Component Age", 20000 ] }, "then": 20000 },
+                        { "case": { "$lt": [ "$Component Age", 25000 ] }, "then": 25000 },
+                        { "case": { "$lt": [ "$Component Age", 30000 ] }, "then": 30000 },
+                        { "case": { "$lt": [ "$Component Age", 35000 ] }, "then": 35000 },
+                        { "case": { "$lt": [ "$Component Age", 40000 ] }, "then": 40000 },
+                        { "case": { "$lt": [ "$Component Age", 45000 ] }, "then": 45000 },
+                        { "case": { "$lt": [ "$Component Age", 50000 ] }, "then": 50000 },
+                        { "case": { "$lt": [ "$Component Age", 55000 ] }, "then": 55000 },
+                        { "case": { "$lt": [ "$Component Age", 60000 ] }, "then": 60000 },
+                        { "case": { "$lt": [ "$Component Age", 65000 ] }, "then": 65000 },
+                        { "case": { "$lt": [ "$Component Age", 70000 ] }, "then": 70000 },
+                        { "case": { "$lt": [ "$Component Age", 75000 ] }, "then": 75000 },
+                        { "case": { "$lt": [ "$Component Age", 80000 ] }, "then": 80000 },
+                        { "case": { "$gt": [ "$Component Age", 80000 ] }, "then": "Others" }
+                      ]
+                    }             
+                    
+                 }
+            },
+            "count": { "$sum": 1 }
+          }},
+          {
+            "$sort": {
+                "RemoveDate": 1
             }
-
+          }
         ]).then((result) => {
             res.send(result)
         }).catch((e) => {
             res.send(e)
         })
+        
+        
+       
+          
     }
 
 
