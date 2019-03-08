545da8fe9375e84a16f59d53db3da7233d953677
Danylo Denysov <danyldezwaan@hotmail.com>
2019-03-08 16:37:18 +1000

    changed get request to post request

--- dingo-server/controller/dataController.js
+++ dingo-server/controller/dataController.js
@@ -1,63 +1,65 @@
 module.exports = (Data) => {
     // Retrieves the filter field data for particular site
-   const filter = (req, res) =>{
-    Data.aggregate([
-                { 
-                    "$match" : {
-                        "Site" : "Newmont Nevada"
-                    }
-                },
-                {    $group: {
-                                _id: null,
-                              "Location"  : {$addToSet: '$Location'},
-                              "Global Asset Make": {$addToSet: "$Global Asset Make"},
-                              "Global Asset Model": {$addToSet: "$Global Asset Model"},
-                              "Global Asset Type": {$addToSet: "$Global Asset Type"},
-                              "Global Component Type": {$addToSet: '$Global Component Type'},
-                              "Global Component Model":{$addToSet:'$Global Component Model'}
-                     }
+    const filter = (req, res) => {
+        Data.aggregate([
+            {
+                "$match": {
+                    "Site": "Newmont Nevada"
                 }
-            ]).then((result) => {
-            res.send(result)
-            }).catch((e) => {
-            res.send(e)
-            })
-
-
-   }
-
-
-
-
-    //  Retrieves the Component Age and total count (last three year by default)from mongodb Atlas
-    const dashboard =  (req, res) => {
-       Data.aggregate([
-        { $match:  {
-                        
-                       "Site" : "Newmont Nevada",
-                       "$or":[ {"RemoveDate" : 2019},{ "RemoveDate" : 2018},{"RemoveDate" : 2017}]
-                            
-                   }
-        },
-        {
-        $bucket: {
-        groupBy: "$Component Age",
-        boundaries: [0, 2000, 4000, 6000, 8000, 10000, 12000,14000,16000,18000,20000,22000,24000,26000,28000,30000,32000,34000,36000,38000,40000,42000,44000,46000,48000,50000],
-        default: "50000 <",
-        output: {
-        "count": { $sum: 1 }
-               }
+            },
+            {
+                $group: {
+                    _id: null,
+                    "Location": { $addToSet: '$Location' },
+                    "Global Asset Make": { $addToSet: "$Global Asset Make" },
+                    "Global Asset Model": { $addToSet: "$Global Asset Model" },
+                    "Global Asset Type": { $addToSet: "$Global Asset Type" },
+                    "Global Component Type": { $addToSet: '$Global Component Type' },
+                    "Global Component Model": { $addToSet: '$Global Component Model' }
+                }
             }
-        }
         ]).then((result) => {
             res.send(result)
         }).catch((e) => {
             res.send(e)
         })
-        
-        
-       
-          
+
+
+    }
+
+
+
+
+    //  Retrieves the Component Age and total count (last three year by default)from mongodb Atlas
+    const dashboard = (req, res) => {
+        Data.aggregate([
+            {
+                $match: {
+
+                    "Site": "Newmont Nevada",
+                    "$or": [{ "RemoveDate": 2019 }, { "RemoveDate": 2018 }, { "RemoveDate": 2017 }]
+
+                }
+            },
+            {
+                $bucket: {
+                    groupBy: "$Component Age",
+                    boundaries: [0, 2000, 4000, 6000, 8000, 10000, 12000, 14000, 16000, 18000, 20000, 22000, 24000, 26000, 28000, 30000, 32000, 34000, 36000, 38000, 40000, 42000, 44000, 46000, 48000, 50000],
+                    default: "50000 <",
+                    output: {
+                        "count": { $sum: 1 }
+                    }
+                }
+            }
+        ]).then((result) => {
+            res.send(result)
+        }).catch((e) => {
+            res.send(e)
+        })
+
+
+
+
     }
 
 
