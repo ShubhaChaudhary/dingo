3793a861adc328e6b8e0dc0dd142bd063589c51e
Danylo Denysov <danyldezwaan@hotmail.com>
2019-03-08 14:44:35 +1000

    Added post method for the filter

--- dingo-client/src/components/Filter.js
+++ dingo-client/src/components/Filter.js
@@ -7,24 +7,50 @@ import auth from '../redux/reducers/auth'
 import moment from "moment";
 import InputRange from 'react-input-range'
 import 'react-input-range/lib/css/index.css'
+import axios from 'axios'
 
 class Filter extends React.Component {
-    componentDidMount(){
-        if(this.props.access_token){
-            
+    componentDidMount() {
+        if (this.props.access_token) {
+
         }
     }
+
     userLogout = () => {
         console.log(this.props.logout())
         // this.props.logout()
         document.location.reload()
     }
-   render() {
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        const form = event.target
+        let years = []
+
+        for (let i = 0; i <= this.props.year.max - Number(this.props.year.min); i += 1) {
+            years.push({ "RemoveDate": this.props.year.min + i })
+        }
+        let { data } = await axios.post('http://localhost:3001/data/performance', {
+            "Location": form.elements.location.value,
+            "Global Asset Make": form.elements.assetMake.value,
+            "Global Component Model": form.elements.componentModel.value,
+            "Global Asset Type": form.elements.assetType.value,
+            "Global Asset Model": form.elements.assetModel.value,
+            "Global Component Type": form.elements.componentType.value,
+            "Global Component Make": form.elements.componenMake.value
+        })
+
+    }
+
+    render() {
         return (
             <div id="filter-container">
                 <h2>Filter</h2>
                 <hr />
-                <from>
+
+                <form onSubmit={this.handleSubmit}>
+
                     <label for="site">Site:</label><br />
                     <select id="site">
 
@@ -40,54 +66,72 @@ class Filter extends React.Component {
                     </select>
 
                     <br /><br />
-                    <label for="asset-make">Asset Make:</label><br />
-                    <select id="asset-make">
+                    <label for="assetMake">Asset Make:</label><br />
+                    <select id="assetMake">
                         <option>Make 1</option>
                         <option>Make 2</option>
                         <option>Make 3</option>
                     </select>
 
                     <br /><br />
-                    <label for="asset-model">Asset Model:</label><br />
-                    <select id="asset-model">
+                    <label for="assetModel">Asset Model:</label><br />
+                    <select id="assetModel">
                         <option>AAAA</option>
                         <option>BBBB</option>
                         <option>CCCC</option>
                     </select>
 
                     <br /><br />
-                    <label for="component-type">Component Type:</label><br />
-                    <select id="component-type">
-                        <option>Engine</option>
-                        <option>Drill</option>
-                        <option>Drive Train</option>
+                    <label for="assetType">Asset Type:</label><br />
+                    <select id="assetType">
+                        <option>AAAA</option>
+                        <option>BBBB</option>
+                        <option>CCCC</option>
                     </select>
 
                     <br /><br />
-                    <label for="component-model">Component Model:</label><br />
-                    <select id="component-model">
+                    <label for="componenMake">Component Type:</label><br />
+                    <select id="componenMake">
+                        <option>ABC</option>
+                        <option>CDAl</option>
+                        <option>DSA</option>
+                    </select>
+
+                    <br /><br />
+                    <label for="componentModel">Component Model:</label><br />
+                    <select id="componentModel">
                         <option>DDDD</option>
                         <option>EEEE</option>
                         <option>FFFF</option>
                     </select>
 
                     <br /><br />
-                    <form className="form">
-                    <label for="component-model">Year Range selctor:</label><br />
+                    <label for="componentType">Component Type:</label><br />
+                    <select id="componentType">
+                        <option>Engine</option>
+                        <option>Drill</option>
+                        <option>Drive Train</option>
+                    </select>
+
+                    <br /><br />
+
+                    <label for="yearRange">Year Range selctor:</label><br />
                     <br /><br />
                     <InputRange
-                       draggableTrack
+                        draggableTrack
                         maxValue={moment().format('YYYY')}
                         minValue={2000}
                         onChange={value => this.props.setYearRange(value)}
                         onChangeComplete={value => console.log(value)}
-                        value={this.props.year} />
-                   
-                    </form>
-                    <br /><br />
-                    <Button variant="contained" color="primary" >Filter</Button>
+                        value={this.props.year}
+                        id="yearRange" />
 
-                </from>
+
+                    <br /><br />
+
+                    <Button type="submit" variant="contained" color="primary">Filter</Button>
+
+                </form>
                 <br /><br />
                 <Button onClick={this.userLogout} variant="contained" color="primary" >Logout</Button>
             </div>
@@ -97,9 +141,9 @@ class Filter extends React.Component {
 
 const mapStateToProps = state => ({
     site: state.auth.site,
-    access_token:state.auth.access_token,
-    year:state.yearpicker.year,
-    
+    access_token: state.auth.access_token,
+    year: state.yearpicker.year,
+
 })
 export default connect(mapStateToProps, actions)(Filter)
 
